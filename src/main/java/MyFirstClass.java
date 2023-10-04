41ce388f34b956b6fefb57977e291cba3cf094fb
Ellaha Sharif <ellahasharifi33@gmail.com>
2023-10-04 01:03:14 +0100

    changes from new branch

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -1,6 +1,7 @@
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.println("hello class" 
+        System.out.println("hello class"
                 );
+
     }
 }
