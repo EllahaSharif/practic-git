cce5120a71d023841200091873abef0a7b763fc8
EllahaSharif <146789453+EllahaSharif@users.noreply.github.com>
2023-10-04 05:59:00 +0100

    Update MyFirstClass.java

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -2,6 +2,7 @@ public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("hello class"
                 );
-
+        
+system.out.println("Adding by liza");
     }
 }
